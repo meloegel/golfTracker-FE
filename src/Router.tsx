40fba059f8ad307b3367a67b273e4a3c5e7f1099
meloegel <meloegel@gmail.com>
2022-05-30 23:07:36 -0400

    added basic dashboard

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./Dashboard";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Hello World</div>} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );

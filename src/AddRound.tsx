345d63683c5efa2ea5883a51299a83314e4d62ef
meloegel <meloegel@gmail.com>
2022-06-14 23:01:34 -0400

    updates

--- src/AddRound.tsx
+++ src/AddRound.tsx
@@ -2,6 +2,9 @@ export default function AddRound() {
   return (
     <div>
       <h1>Add Round</h1>
+      <form>
+        <input value="" onChange={() => {}} name="" type="text" />
+      </form>
     </div>
   );
 }

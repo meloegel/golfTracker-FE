770fa6577984deedf9d5e9768e7d2a79422e2963
meloegel <meloegel@gmail.com>
2022-06-29 21:25:05 -0400

    added fetch on rounds to dashbaord

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,8 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useFetch from "./hooks/useFetch";
 import RoundCard from "./RoundCard";
 
 export default function Dashboard() {
+  const [request, data] = useFetch<any>();
   const [rounds, setRounds] = useState([] as any[]);
+
+  useEffect(() => {
+    const headers = {
+      "Content-Type": "application/json"
+    }
+    request("http://localhost:8080/rounds", {
+      method: "GET",
+      headers: headers,
+    })
+  }, [request])
+
+  useEffect(() => {
+    if (data) {
+      setRounds(data)
+    }
+  }, [data])
+
 
   return (
     <div>

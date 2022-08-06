580c4ec2b39096a2b8d44d8614fccf62d47cef14
meloegel <meloegel@gmail.com>
2022-08-05 21:12:01 -0400

    adding basic inputs to login

--- src/account/Login.tsx
+++ src/account/Login.tsx
@@ -1,7 +1,14 @@
 export default function Login(): JSX.Element {
+
   return (
-    <>
+    <div>
       <h1>Login Page</h1>
-    </>
+      <form>
+      <input value="username" onChange={() => {}} name="username" type="text" />
+      <input value="password" onChange={() => {}} name="password" type="text" />
+ 
+
+      </form>
+    </div>
   );
 }

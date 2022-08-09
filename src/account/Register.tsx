6c2e422dc749ab22d5630938428c23ea025c5d99
meloegel <meloegel@gmail.com>
2022-08-08 20:33:47 -0400

    adding some inputs to register

--- src/account/Register.tsx
+++ src/account/Register.tsx
@@ -1,5 +1,22 @@
 export default function Register(): JSX.Element {
-    return <div>
-        <h1>Register</h1>
+  return (
+    <div>
+      <h1>Register</h1>
+      <form>
+        <input
+          value="username"
+          onChange={() => {}}
+          name="username"
+          type="text"
+        />
+        <input
+          value="password"
+          onChange={() => {}}
+          name="password"
+          type="text"
+        />
+        <input value="email" onChange={() => {}} name="email" type="text" />
+      </form>
     </div>
+  );
 }
